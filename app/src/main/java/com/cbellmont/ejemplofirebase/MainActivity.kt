82fa1e47b02d4d1e00572b085f94f5711a6acfd1
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-08-30 22:42:38 +0200

    Basic integration

--- app/src/main/java/com/cbellmont/ejemplofirebase/MainActivity.kt
+++ app/src/main/java/com/cbellmont/ejemplofirebase/MainActivity.kt
@@ -2,10 +2,21 @@ package com.cbellmont.ejemplofirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        buttonRegister.setOnClickListener {
+            firebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, Bundle())
+        }
     }
+
 }
