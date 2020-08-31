38c3b985976c95899bb4e291d3473efef3f07f40
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-08-31 14:10:59 +0200

    Notifications

--- app/src/main/java/com/cbellmont/ejemplofirebase/MainActivity.kt
+++ app/src/main/java/com/cbellmont/ejemplofirebase/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.cbellmont.ejemplofirebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
 import java.lang.Exception
 import kotlin.random.Random
 
@@ -12,14 +21,55 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var firebaseAnalytics: FirebaseAnalytics
 
+    companion object {
+        private val TAG = MainActivity::class.java.name
+    }
     private var userId = Random.nextInt()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val channelId = getString(R.string.default_channel_id)
+            val channelName = "Mis Notificaciones"
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(
+                NotificationChannel(channelId,
+                channelName, NotificationManager.IMPORTANCE_HIGH)
+            )
+        }
+
         buttonRegister.setOnClickListener {
             firebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, Bundle())
+
+            FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "Fallo al obtener la instancia", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+                    token?.let { Log.d(TAG, "Token = $it") }
+                    Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+                })
+        }
+
+        buttonListenTopic.setOnClickListener {
+            FirebaseMessaging.getInstance().subscribeToTopic(getString(R.string.custom_topic))
+                .addOnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        val msg = "Fracaso escuchando a ${getString(R.string.custom_topic)}"
+                        Log.e(TAG, msg)
+                    } else {
+                        val msg = "Exito escuchando a ${getString(R.string.custom_topic)}"
+                        Log.d(TAG, msg)
+                    }
+                }
         }
         buttonDoCrash.setOnClickListener {
             val nullString : String? = null
---
+++ app/src/main/java/com/cbellmont/ejemplofirebase/MainService.kt
@@ -0,0 +1,66 @@
+package com.cbellmont.ejemplofirebase
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MainService : FirebaseMessagingService(){
+
+    companion object {
+        private val TAG = MainService::class.java.name
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Contenido del mensaje: ${remoteMessage.data}")
+        }
+
+        remoteMessage.notification?.body?.let {
+            Log.d(TAG, "Cuerpo del mensaje: $it")
+            //sendNotification(it)
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d(TAG, "token --> $token")
+    }
+
+
+    // OPCIONAL : Utilizar solamente si quieres lanzar una notificaion personalizada
+    /* private fun sendNotification(messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.putExtra("AAA", "aaaa")
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val channelId = getString(R.string.default_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    } */
+
+}
